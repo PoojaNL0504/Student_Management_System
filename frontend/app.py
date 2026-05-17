2fe243761aab81ed6ddc4ac47b574666de10c509
PoojaNL0504 <poojanl.work5@gmail.com>
2026-05-17 19:19:19 +0530

    url updation

--- frontend/app.py
+++ frontend/app.py
@@ -60,7 +60,7 @@ if st.session_state.page == "signup":
             st.error("All fields required ❌")
         else:
             requests.post(
-                f"{API_URL}/signup",
+                f"{API_URL}/auth/signup",
                 json={"username": username, "password": password}
             )
             st.success("Signup successful ✅")
@@ -81,7 +81,7 @@ elif st.session_state.page == "login":
 
     if st.button("Login 🔑"):
         res = requests.post(
-            f"{API_URL}/login",
+            f"{API_URL}/auth/login",
             json={"username": username, "password": password}
         )
         st.write(res.status_code)
